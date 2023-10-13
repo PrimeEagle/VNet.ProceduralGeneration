052506d0f78379e08cd21bf7c0a100deffd8bdae
PrimeEagle <misc@hiddendragon.com>
2023-10-13 03:04:10 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.ProceduralGeneration@1370 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.ProceduralGeneration/Cosmological/Contexts/ContextBase.cs
+++ VNet.ProceduralGeneration/Cosmological/Contexts/ContextBase.cs
@@ -1,28 +1,32 @@
 ﻿using System.Numerics;
 using VNet.ProceduralGeneration.Cosmological.AstronomicalObjects;
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace VNet.ProceduralGeneration.Cosmological.Contexts
 {
     public  abstract class ContextBase
     {
         public float Age { get; set; }                      // years
+        public float Lifespan { get; set; }                 // years
         public double Mass { get; set; }                    // kg
-        public double Size { get; set; }                     // AU
-        public float AbsoluteMagnitude { get; set; }        // 
-        public float Luminosity { get; set; }              //  L⊙
+        public float Diameter { get; set; }                 // AU
         public float Temperature { get; set; }              // Kelvin
+        public float Luminosity { get; set; }               //  L⊙
         public Vector3 Position { get; set; }               // AU
+        public Vector3 Orientation { get; set; }
 
 
 
         protected virtual void LoadBaseProperties(AstronomicalObject astronomicalObject)
         {
             this.Age= astronomicalObject.Age;
+            this.Lifespan = astronomicalObject.Lifespan;
             this.Mass = astronomicalObject.Mass;
-            this.Size = astronomicalObject.Size;
-            this.AbsoluteMagnitude = astronomicalObject.AbsoluteMagnitude;
+            this.Diameter = astronomicalObject.Diameter;
             this.Temperature = astronomicalObject.Temperature;
+            this.Luminosity = astronomicalObject.Luminosity;
             this.Position = astronomicalObject.Position;
+            this.Orientation = astronomicalObject.Orientation;
         }
     }
 }
