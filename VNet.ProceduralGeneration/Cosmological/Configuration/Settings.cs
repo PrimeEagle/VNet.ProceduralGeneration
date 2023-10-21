4fd9c4ceb4c7801fdd99faad8b5626eba0bf5352
PrimeEagle <misc@hiddendragon.com>
2023-10-21 02:29:17 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.ProceduralGeneration@1416 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.ProceduralGeneration/Cosmological/Configuration/Settings.cs
+++ VNet.ProceduralGeneration/Cosmological/Configuration/Settings.cs
@@ -1,13 +1,15 @@
-﻿using VNet.Configuration;
+﻿using System.Collections.ObjectModel;
+using VNet.Configuration;
 
 namespace VNet.ProceduralGeneration.Cosmological.Configuration;
 
 public class Settings : ISettings
 {
+
     public AdvancedSettings Advanced { get; init; }
 
     public BasicSettings Basic { get; init; }
-
+    public ObservableCollection<string> TabNames { get; } = new ObservableCollection<string> { "One", "Two", "Three", "Four" };
     public Settings()
     {
         Basic = new BasicSettings();
